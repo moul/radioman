4a4885cf1eb91b4f87ae8ad7124353580791d228
Manfred Touron <m@42.am>
2015-12-13 22:53:28 +0100

    Add authentication on admin pages (fix #5)

--- radioman/cmd/radiomand/main.go
+++ radioman/cmd/radiomand/main.go
@@ -25,8 +25,16 @@ func main() {
 
 	// Setup the web server
 	router := gin.Default()
+	public := router.Group("/")
+	admin := router.Group("/")
+	liquidsoap := router.Group("/")
+	// Admin auth
+	// FIXME: make accounts dynamic
+	accounts := gin.Accounts{"admin": "admin"}
+	admin.Use(gin.BasicAuth(accounts))
+	// FIXME: add authentication on liquidsoap next handler
 
-	router.GET("/ping", func(c *gin.Context) {
+	public.GET("/ping", func(c *gin.Context) {
 		c.String(200, "pong")
 	})
 
@@ -34,36 +42,37 @@ func main() {
 	if os.Getenv("WEBDIR") != "" {
 		staticPrefix = os.Getenv("WEBDIR")
 	}
-	router.StaticFile("/", path.Join(staticPrefix, "static/index.html"))
-	router.StaticFile("/admin/", path.Join(staticPrefix, "static/admin/index.html"))
-	router.Static("/static", path.Join(staticPrefix, "static"))
-	router.Static("/bower_components", path.Join(staticPrefix, "bower_components"))
 
-	router.GET("/api/playlists", playlistsEndpoint)
-	router.GET("/api/playlists/:name", playlistDetailEndpoint)
-	router.PATCH("/api/playlists/:name", playlistUpdateEndpoint)
-	router.GET("/api/playlists/:name/tracks", playlistTracksEndpoint)
+	public.StaticFile("/", path.Join(staticPrefix, "static/index.html"))
+	public.Static("/static", path.Join(staticPrefix, "static"))
+	public.Static("/bower_components", path.Join(staticPrefix, "bower_components"))
 
-	router.GET("/api/radios/default", defaultRadioEndpoint)
-	router.GET("/api/radios/default/endpoints", radioEndpointsEndpoint)
-	router.POST("/api/radios/default/skip-song", radioSkipSongEndpoint)
-	router.POST("/api/radios/default/play-track", radioPlayTrackEndpoint)
-	router.POST("/api/radios/default/set-next-track", radioSetNextTrackEndpoint)
+	admin.StaticFile("/admin/", path.Join(staticPrefix, "static/admin/index.html"))
 
-	router.GET("/api/tracks/:hash", trackDetailEndpoint)
+	admin.GET("/api/playlists", playlistsEndpoint)
+	admin.GET("/api/playlists/:name", playlistDetailEndpoint)
+	admin.PATCH("/api/playlists/:name", playlistUpdateEndpoint)
+	admin.GET("/api/playlists/:name/tracks", playlistTracksEndpoint)
 
-	router.GET("/api/liquidsoap/getNextSong", getNextSongEndpoint)
+	admin.GET("/api/radios/default", defaultRadioEndpoint)
+	public.GET("/api/radios/default/endpoints", radioEndpointsEndpoint)
+	admin.POST("/api/radios/default/skip-song", radioSkipSongEndpoint)
+	admin.POST("/api/radios/default/play-track", radioPlayTrackEndpoint)
+	admin.POST("/api/radios/default/set-next-track", radioSetNextTrackEndpoint)
 
-	router.GET("/playlist.m3u", m3uPlaylistEndpoint)
+	admin.GET("/api/tracks/:hash", trackDetailEndpoint)
 
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8000"
-	}
+	liquidsoap.GET("/api/liquidsoap/getNextSong", getNextSongEndpoint)
+
+	public.GET("/playlist.m3u", m3uPlaylistEndpoint)
 
 	// Launch routines
 	go Radio.UpdatePlaylistsRoutine()
 
 	// Start web server mainloop
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8000"
+	}
 	router.Run(fmt.Sprintf(":%s", port))
 }
