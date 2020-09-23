c3caf6fae9d3793460257e307003d3ecfb60bca0
Manfred Touron <94029+moul@users.noreply.github.com>
2020-09-23 22:21:45 +0200

    feat: refactor

--- radioman/cmd/radiomand/main.go
+++ radioman/cmd/radiomand/main.go
@@ -1,78 +1,50 @@
 package main
 
 import (
+	"context"
+	"flag"
 	"fmt"
+	"log"
 	"os"
-	"path"
 
-	"github.com/Sirupsen/logrus"
-	"github.com/gin-gonic/gin"
 	"github.com/moul/radioman/radioman/pkg/radioman"
+	"github.com/peterbourgon/ff/v3/ffcli"
+	"moul.io/motd"
 )
 
-var Radio *radioman.Radio
+var opts radioman.Opts
 
 func main() {
-	// Setup the Radio instance
-	Radio = radioman.NewRadio("RadioMan")
-
-	if err := Radio.Init(); err != nil {
-		logrus.Fatalf("Failed to initialize the radio: %v", err)
+	if err := run(os.Args); err != nil {
+		if err != flag.ErrHelp {
+			log.Fatalf("error: %v", err)
+		}
+		os.Exit(1)
 	}
-	if err := Radio.StdPopulate(); err != nil {
-		logrus.Fatalf("Failed to populate the radio: %v", err)
+}
+
+func run(args []string) error {
+	// flags
+	fs := flag.NewFlagSet("radioman", flag.ExitOnError)
+	fs.BoolVar(&opts.Verbose, "v", false, "verbose")
+
+	root := &ffcli.Command{
+		ShortUsage: "radioman <subcommand> [flags]",
+		Exec: func(ctx context.Context, args []string) error {
+			fmt.Println(motd.Default())
+			man, err := radioman.New(opts)
+			if err != nil {
+				return fmt.Errorf("radioman.New: %w", err)
+			}
+
+			err = man.Start()
+			if err != nil {
+				return fmt.Errorf("radioman.Start: %w", err)
+			}
+
+			return nil
+		},
 	}
 
-	// Setup the web server
-	router := gin.Default()
-	public := router.Group("/")
-	admin := router.Group("/")
-	liquidsoap := router.Group("/")
-	// Admin auth
-	// FIXME: make accounts dynamic
-	accounts := gin.Accounts{"admin": "admin"}
-	admin.Use(gin.BasicAuth(accounts))
-	// FIXME: add authentication on liquidsoap next handler
-
-	public.GET("/ping", func(c *gin.Context) {
-		c.String(200, "pong")
-	})
-
-	staticPrefix := "./radioman/web"
-	if os.Getenv("WEBDIR") != "" {
-		staticPrefix = os.Getenv("WEBDIR")
-	}
-
-	public.StaticFile("/", path.Join(staticPrefix, "static/index.html"))
-	public.Static("/static", path.Join(staticPrefix, "static"))
-	public.Static("/bower_components", path.Join(staticPrefix, "bower_components"))
-
-	admin.StaticFile("/admin/", path.Join(staticPrefix, "static/admin/index.html"))
-
-	admin.GET("/api/playlists", playlistsEndpoint)
-	admin.GET("/api/playlists/:name", playlistDetailEndpoint)
-	admin.PATCH("/api/playlists/:name", playlistUpdateEndpoint)
-	admin.GET("/api/playlists/:name/tracks", playlistTracksEndpoint)
-
-	admin.GET("/api/radios/default", defaultRadioEndpoint)
-	public.GET("/api/radios/default/endpoints", radioEndpointsEndpoint)
-	admin.POST("/api/radios/default/skip-song", radioSkipSongEndpoint)
-	admin.POST("/api/radios/default/play-track", radioPlayTrackEndpoint)
-	admin.POST("/api/radios/default/set-next-track", radioSetNextTrackEndpoint)
-
-	admin.GET("/api/tracks/:hash", trackDetailEndpoint)
-
-	liquidsoap.GET("/api/liquidsoap/getNextSong", getNextSongEndpoint)
-
-	public.GET("/playlist.m3u", m3uPlaylistEndpoint)
-
-	// Launch routines
-	go Radio.UpdatePlaylistsRoutine()
-
-	// Start web server mainloop
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8000"
-	}
-	router.Run(fmt.Sprintf(":%s", port))
+	return root.ParseAndRun(context.Background(), args[1:])
 }
--- radioman/go.mod
+++ radioman/go.mod
@@ -9,9 +9,12 @@ require (
 	github.com/manucorporat/sse v0.0.0-20150715184805-fe6ea2c8e398 // indirect
 	github.com/mattn/go-colorable v0.0.0-20150625154642-40e4aedc8fab // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/peterbourgon/ff v1.7.0
+	github.com/peterbourgon/ff/v3 v3.0.0
 	github.com/stretchr/testify v1.6.1 // indirect
 	github.com/wtolson/go-taglib v0.0.0-20151114194754-6e68349ff94e
 	golang.org/x/net v0.0.0-20150824032521-7654728e3819 // indirect
 	gopkg.in/bluesuncorp/validator.v5 v5.10.4-0.20150924114428-d5acf1dac437 // indirect
 	gopkg.in/go-playground/validator.v5 v5.10.3 // indirect
+	moul.io/motd v1.0.0
 )
